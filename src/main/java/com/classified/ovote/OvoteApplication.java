3b51b89d1d3542f3d16ab39d8de96b449e12f32b
ogomb <lewis.mbogo@andela.com>
2019-04-25 09:25:00 +0300

    create project init.

---
+++ src/main/java/com/classified/ovote/OvoteApplication.java
@@ -0,0 +1,13 @@
+package com.classified.ovote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvoteApplication.class, args);
+	}
+
+}
